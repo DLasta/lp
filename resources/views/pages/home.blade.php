761c2bdad4eb1ac994c6e66cb20d6273e8593049
DLasta <dario.lasta123@gmail.com>
2023-09-14 23:06:43 +0200

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts')
+@section('content')
+  <header class="p-5 headerImage d-flex align-items-center ">
+    <div class="container text-center text-white ">
+      <h1>TKO je odvažan, taj dobiva</h1>
+      <button type="button" class="btn btn-danger btn-lg  m-3">Zaigraj</button>
+    </div>
+  </header>
+@endsection
